bc67df3657cdc28821bed8fc3b04a582a4f7d9ca
dinesh barathi <dinesh.barathi@teliacompany.com>
2022-10-09 10:42:11 +0530

    Newly helloworld files

---
+++ FirstHelloWorld.java
@@ -0,0 +1,14 @@
+package com.dinesh.barathi;
+
+public class FirstHelloWorld {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("Welocme to new helloworld");
+		System.out.println("The value of C is" +c);
+
+	}
+
+}
